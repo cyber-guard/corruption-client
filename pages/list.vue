6223577eef484375e1d4de98aa5b093c14981453
Simon <contact@cyber-guard.co.uk>
2020-05-13 10:45:39 +0200

    Improve list items and add Citations

--- pages/list.vue
+++ pages/list.vue
@@ -13,7 +13,7 @@
         </mdb-card>
       </mdb-col>
     </mdb-row>
-    <mdb-row v-for="article in articles" :key="article" class="item">
+    <mdb-row v-for="article in articles" :key="article.Id" class="item">
       <mdb-col col="12">
         <mdb-card class="mt-2">
           <mdb-card-body cascade>
@@ -24,23 +24,64 @@
                 >
                 <h5 class="indigo-text">
                   <strong
-                    >{{ article.Author }}, {{ article.Year }},
+                    >{{ article.Authors }}, {{ article.Year }},
                     {{ article.Source }}</strong
                   >
                 </h5>
                 <mdb-card-text>{{ article.Abstract }}</mdb-card-text>
               </mdb-col>
               <mdb-col col="4">
-                <mdb-card-title><strong>Share</strong></mdb-card-title>
-                <a class="px-2 fa-lg li-ic"> <mdb-icon fab icon="linkedin"/></a>
-                <a class="px-2 fa-lg tw-ic"> <mdb-icon fab icon="twitter"/></a>
-                <a class="px-2 fa-lg fb-ic"> <mdb-icon fab icon="facebook"/></a>
+                <mdb-row>
+                  <mdb-col col="6">
+                    <strong>Subject</strong>
+                  </mdb-col>
+                  <mdb-col col="6">
+                    {{ article.Subject }}
+                  </mdb-col>
+                </mdb-row>
+                <mdb-row class="mt-2">
+                  <mdb-col col="6">
+                    <strong>Share</strong>
+                  </mdb-col>
+                  <mdb-col col="6">
+                    <a class="px-2 fa-lg li-ic">
+                      <mdb-icon fab icon="linkedin"
+                    /></a>
+                    <a class="px-2 fa-lg tw-ic">
+                      <mdb-icon fab icon="twitter"
+                    /></a>
+                    <a class="px-2 fa-lg fb-ic">
+                      <mdb-icon fab icon="facebook"
+                    /></a>
+                  </mdb-col>
+                </mdb-row>
+                <mdb-row class="mt-2">
+                  <mdb-col col="6" class="text-center">
+                    <button
+                      class="btn btn-primary"
+                      @click="citation(article.OA)"
+                    >
+                      Citation
+                    </button>
+                  </mdb-col>
+                  <mdb-col col="6" class="text-center" v-html="article.OA">
+                    {{ article.OA }}
+                  </mdb-col>
+                </mdb-row>
               </mdb-col>
             </mdb-row>
           </mdb-card-body>
         </mdb-card>
       </mdb-col>
     </mdb-row>
+    <mdb-modal :show="modal" :centered="true" @close="modal = false">
+      <mdb-modal-body v-html="citation_content">{{
+        citation_content
+      }}</mdb-modal-body>
+      <mdb-modal-footer>
+        <mdb-btn color="secondary" @click.native="modal = false">Close</mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
   </mdb-container>
 </template>
 
@@ -54,10 +95,15 @@ import {
   mdbCard,
   mdbCardText,
   mdbCardBody,
-  mdbCardTitle
+  mdbCardTitle,
+  mdbBtn,
+  mdbModal,
+  mdbModalBody,
+  mdbModalFooter
 } from 'mdbvue'
+import Cite from 'citation-js'
 export default {
-  name: 'Input',
+  name: 'ListOfValues',
   components: {
     mdbContainer,
     mdbRow,
@@ -67,13 +113,18 @@ export default {
     mdbCard,
     mdbCardText,
     mdbCardBody,
-    mdbCardTitle
+    mdbCardTitle,
+    mdbBtn,
+    mdbModal,
+    mdbModalBody,
+    mdbModalFooter
   },
   async fetch() {
     const data = await this.$axios.$get('/articles', {
       params: {
         _limit: 10,
         Abstract_contains: this.value,
+        Authors_contains: this.value,
         Title_contains: this.value
       }
     })
@@ -83,7 +134,22 @@ export default {
   data() {
     return {
       value: '',
+      modal: false,
+      citation_content: '',
       articles: []
+    }
+  },
+  methods: {
+    citation(url) {
+      const doi = url.match(/href="(.*?)"/)
+      const cite = new Cite('10.1007/978-3-030-14140-0_1')
+      this.modal = true
+      this.citation_content = cite.format('bibliography', {
+        format: 'html',
+        template: 'apa',
+        lang: 'en-US'
+      })
+      console.log(doi)
     }
   }
 }
